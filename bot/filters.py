5eceefbbea31c921ee2fa1ddeaeddf73a9a3f4d5
Evgeniy Dubovskoy <edubovskoy@gmail.com>
2018-07-08 02:47:00 +0300

    fix filter

--- bot/filters.py
+++ bot/filters.py
@@ -7,12 +7,13 @@ class MentionFilter(BaseFilter):
     """
     Filter for messages, which is targeted to specific contact
     """
-    name = 'filters.BotContact'
+    name = 'filters.MentionFilter'
 
     def __init__(self, contact_name: str):
         self.contact_name = contact_name
 
     def filter(self, message):
-        mention_pattern = re.compile(f'@{self.contact_name} .+')
-        return mention_pattern.match(message.text)
-
+        if message.text:
+            mention_pattern = re.compile(f'@{self.contact_name} .+')
+            return mention_pattern.match(message.text)
+        return False
