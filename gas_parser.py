2e92fa61541ac15adb927ef8cdd6b933116e0e94
Evgeniy Dubovskoy <edubovskoy@gmail.com>
2019-01-16 01:12:19 +0200

    update data source for gas to minfin.com.ua

--- gas_parser.py
+++ gas_parser.py
@@ -3,32 +3,32 @@ import requests
 
 
 def get_gas_prices():
-    url = 'https://avtomaniya.com/site/fuel-dnepr'
+    url = 'https://index.minfin.com.ua/markets/fuel/reg/Днепропетровская'
 
-    target_headers = ['АЗС', 'А95', 'А92', 'Газ']
-    target_stations = ['Авиас', 'Укрнафта', 'ОККО', 'WOG', 'Glusco']
-    headers_map = {}
+    target_stations = ['Авиас', 'Укрнафта', 'ОККО', 'WOG', 'Glusco', 'SKY', 'БРСМ-Нафта']
+    excluded_gas_prices = ['А\xa092']
 
     r = requests.get(url)
     tree = document_fromstring(r.text)
 
+    tree.xpath('//table [./caption [contains(text(), "цены операторов")]]')
+
     gas_prices = []
 
-    _headers = tree.xpath('//div [@class="prices-for-kiev for-kiev"]/table'
-                          '/tr [@class="legend-table header-table"]//text() [normalize-space()]')
-    for i, header in enumerate(_headers):
-        if header in target_headers:
-            headers_map[header] = i
+    headers_map = {
+        header.replace('\xa0', ' '): i for i, header
+        in enumerate(tree.xpath('//table [./caption [contains(text(), "цены операторов")]]//th//text()'))
+        if header not in excluded_gas_prices
+    }
 
-    headers = [header for header in _headers if header in target_headers]
-    gas_prices.append(' / '.join(headers))
+    gas_prices.append(' / '.join(headers_map))
 
-    for gas_price in tree.xpath('//div [@class="prices-for-kiev for-kiev"]/table/tr')[1:-1]:
+    for gas_price in tree.xpath('//table [./caption [contains(text(), "цены операторов")]]/tr')[1:]:
         _values = gas_price.xpath('./td//text() [normalize-space()]')
         prices = []
         if _values[0] not in target_stations:
             continue
-        for h in target_headers:
+        for h in headers_map:
             try:
                 prices.append(_values[headers_map[h]])
             except IndexError:
